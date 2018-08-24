e65e1fe782afab2a075c60600d9d0a97bf3ee241
chenshuai <cs4380@163.com>
2018-08-24 08:36:46 +0800

    新增：新增经纬度计算工具类(后期扩充可以计算两点之间的距离)

---
+++ scp-modules/scp-cgp/src/main/java/com/bjzhianjia/scp/cgp/util/LocationUtils.java
@@ -0,0 +1,50 @@
+package com.bjzhianjia.scp.cgp.util;
+
+import com.alibaba.fastjson.JSONObject;
+
+/**
+ * 经纬度工具类
+ * 
+ * @author chenshuai
+ *
+ */
+public class LocationUtils {
+	private static double EARTH_RADIUS = 6378.137;
+
+	private static double rad(double d) {
+		return d * Math.PI / 180.0;
+	}
+
+	/**
+	 * 通过经纬度获取距离(单位：米)
+	 * 
+	 * @param lat1 纬度1
+	 * @param lng1 经度1
+	 * @param lat2 纬度2
+	 * @param lng2 经度2
+	 * @return
+	 * 		相差米数
+	 */
+	public static double getDistance(double lat1, double lng1, double lat2, double lng2) {
+		double radLat1 = rad(lat1);
+		double radLat2 = rad(lat2);
+		double a = radLat1 - radLat2;
+		double b = rad(lng1) - rad(lng2);
+		double s = 2 * Math.asin(Math.sqrt(
+				Math.pow(Math.sin(a / 2), 2) + Math.cos(radLat1) * Math.cos(radLat2) * Math.pow(Math.sin(b / 2), 2)));
+		s = s * EARTH_RADIUS;
+		s = Math.round(s * 10000d) / 10000d;
+		s = s * 1000;
+		return s;
+	}
+
+	public static void main(String[] args) {
+		JSONObject json = JSONObject.parseObject("{\"lng\":116.514134,\"lat\":39.816509}");
+		JSONObject json2 = JSONObject.parseObject("{\"lng\":116.506851,\"lat\":39.812801}");
+		
+		double mishu = LocationUtils.getDistance(json.getDoubleValue("lat"), json.getDoubleValue("lng"),
+			json2.getDoubleValue("lat"), json2.getDoubleValue("lng"));
+		System.out.println("相差米数："+mishu);
+		
+	}
+}
