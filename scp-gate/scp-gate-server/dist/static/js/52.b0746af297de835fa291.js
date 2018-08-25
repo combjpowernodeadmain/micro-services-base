525bf3712be1c80a6af5acea34a2f2ca6790d2e3
bozch <bozch@163.com>
2018-08-25 10:20:55 +0800

    remove unused files

--- scp-gate/scp-gate-server/dist/static/js/52.b0746af297de835fa291.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([52],{lsWi:function(e,t,r){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var l={render:function(){var e=this.$createElement;return(this._self._c||e)("iframe",{attrs:{src:"http://localhost:9422",id:"show",width:"100%",height:"780px",frameborder:"0",name:"showHere"}})},staticRenderFns:[]},n=r("VU/8")(null,l,!1,null,null,null);t.default=n.exports}});
