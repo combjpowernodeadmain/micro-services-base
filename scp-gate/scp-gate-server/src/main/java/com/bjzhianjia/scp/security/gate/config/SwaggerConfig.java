786e4778f032a83169150b77eaabdf7c0d269a80
bozch <bozch@163.com>
2018-08-25 16:08:03 +0800

    remove unused imports

--- scp-gate/scp-gate-server/src/main/java/com/bjzhianjia/scp/security/gate/config/SwaggerConfig.java
+++ scp-gate/scp-gate-server/src/main/java/com/bjzhianjia/scp/security/gate/config/SwaggerConfig.java
@@ -3,18 +3,9 @@ package com.bjzhianjia.scp.security.gate.config;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.ParameterBuilder;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.schema.ModelRef;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Parameter;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger.web.SwaggerResource;
 import springfox.documentation.swagger.web.SwaggerResourcesProvider;
 
