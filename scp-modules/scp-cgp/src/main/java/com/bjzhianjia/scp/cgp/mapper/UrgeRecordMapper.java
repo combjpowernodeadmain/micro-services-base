059981ee2e3cbc5c3311adf7858b5d278c68c5ec
chenshuai <cs4380@163.com>
2018-08-27 21:41:07 +0800

    新增：添加督办查办

---
+++ scp-modules/scp-cgp/src/main/java/com/bjzhianjia/scp/cgp/mapper/UrgeRecordMapper.java
@@ -0,0 +1,15 @@
+package com.bjzhianjia.scp.cgp.mapper;
+
+import com.bjzhianjia.scp.cgp.entity.UrgeRecord;
+import com.bjzhianjia.scp.security.common.mapper.CommonMapper;
+
+/**
+ * 催办记录表
+ * 
+ * @author chenshuai
+ * @email 
+ * @version 2018-08-26 09:02:28
+ */
+public interface UrgeRecordMapper extends CommonMapper<UrgeRecord> {
+	
+}
