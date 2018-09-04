e9a8da6d6bb1fcb3d4b0c5f14e30d98cda135256
尚村山夫 <13821211755@163.com>
2018-09-04 12:41:32 +0800

    1 分页查询网格员

--- scp-modules/scp-cgp/src/main/java/com/bjzhianjia/scp/cgp/rest/AreaGridMemberController.java
+++ scp-modules/scp-cgp/src/main/java/com/bjzhianjia/scp/cgp/rest/AreaGridMemberController.java
@@ -1,10 +1,18 @@
 package com.bjzhianjia.scp.cgp.rest;
 
+import com.bjzhianjia.scp.security.common.msg.TableResultResponse;
 import com.bjzhianjia.scp.security.common.rest.BaseController;
+
+import io.swagger.annotations.ApiOperation;
+
+import com.alibaba.fastjson.JSONObject;
 import com.bjzhianjia.scp.cgp.biz.AreaGridMemberBiz;
 import com.bjzhianjia.scp.cgp.entity.AreaGridMember;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import com.bjzhianjia.scp.security.auth.client.annotation.CheckClientToken;
 import com.bjzhianjia.scp.security.auth.client.annotation.CheckUserToken;
 
@@ -12,6 +20,19 @@ import com.bjzhianjia.scp.security.auth.client.annotation.CheckUserToken;
 @RequestMapping("areaGridMember")
 @CheckClientToken
 @CheckUserToken
-public class AreaGridMemberController extends BaseController<AreaGridMemberBiz,AreaGridMember,Integer> {
+public class AreaGridMemberController
+    extends BaseController<AreaGridMemberBiz, AreaGridMember, Integer> {
 
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    @ApiOperation("分页获取记录")
+    public TableResultResponse<JSONObject> getList(
+        @RequestParam(value = "memId", defaultValue = "") String memId,
+        @RequestParam(value = "page", defaultValue = "1") Integer page,
+        @RequestParam(value = "limit", defaultValue = "10") Integer limit) {
+
+        AreaGridMember areaGridMember = new AreaGridMember();
+        areaGridMember.setGridMember(memId);
+
+        return this.baseBiz.getList(areaGridMember, page, limit);
+    }
 }
