cd729ef4bfdb323bc32c9f574f5814928e41582c
尚村山夫 <13821211755@163.com>
2018-09-04 21:45:15 +0800

    案件类添加完成状态属性 分页查询案件

--- scp-modules/scp-cgp/src/main/java/com/bjzhianjia/scp/cgp/biz/CaseRegistrationBiz.java
+++ scp-modules/scp-cgp/src/main/java/com/bjzhianjia/scp/cgp/biz/CaseRegistrationBiz.java
@@ -14,6 +14,7 @@ import com.bjzhianjia.scp.cgp.entity.CLEConcernedPerson;
 import com.bjzhianjia.scp.cgp.entity.CaseRegistration;
 import com.bjzhianjia.scp.cgp.entity.Constances;
 import com.bjzhianjia.scp.cgp.entity.Result;
+import com.bjzhianjia.scp.cgp.entity.WritsInstances;
 import com.bjzhianjia.scp.cgp.mapper.CaseRegistrationMapper;
 import com.bjzhianjia.scp.cgp.vo.CaseRegistrationVo;
 import com.bjzhianjia.scp.security.common.biz.BusinessBiz;
@@ -34,124 +35,180 @@ import tk.mybatis.mapper.entity.Example.Criteria;
  */
 @Service
 public class CaseRegistrationBiz extends BusinessBiz<CaseRegistrationMapper, CaseRegistration> {
-	@Autowired
-	private CLEConcernedCompanyBiz cLEConcernedCompanyBiz;
-	@Autowired
-	private CLEConcernedPersonBiz cLEConcernedPersonBiz;
 
-	/**
-	 * 添加立案记录，如果有当事人，则一并添加当事人
-	 * 
-	 * @author 尚
-	 * @param procBizData
-	 */
-	public Result<Void> addCase(JSONObject caseRegJObj) {
-		Result<Void> result = new Result<>();
+    @Autowired
+    private CLEConcernedCompanyBiz cLEConcernedCompanyBiz;
 
-		// 添加当事人
-		int concernedId = addConcerned(caseRegJObj);
+    @Autowired
+    private CLEConcernedPersonBiz cLEConcernedPersonBiz;
 
-		// 添加立案单
-		CaseRegistration caseRegistration = JSON.parseObject(caseRegJObj.toJSONString(), CaseRegistration.class);
-		// 生成caseRegistration主键
-		String id = UUIDUtils.generateUuid();
-		caseRegistration.setId(id);
-		// 当事人主键
-		if (concernedId != -1) {
-			caseRegistration.setConcernedId(concernedId);
-		}
+    @Autowired
+    private WritsInstancesBiz writsInstancesBiz;
 
-		this.insertSelective(caseRegistration);
-		// 将生成的立案ID装入procBizData带回工作流，在工作流中会对procBizId属性进行是否为“-1”的判断，如果是“-1”，将用该ID替换“-1”
-		caseRegJObj.put("procBizId", id);
+    /**
+     * 添加立案记录<br/>
+     * 如果 有当事人，则一并添加<br/>
+     * 如果 有文书，则一并添加
+     * 
+     * @author 尚
+     * @param procBizData
+     */
+    public Result<Void> addCase(JSONObject caseRegJObj) {
+        Result<Void> result = new Result<>();
 
-		result.setIsSuccess(true);
-		return result;
-	}
+        // 添加当事人
+        int concernedId = addConcerned(caseRegJObj);
 
-	/**
-	 * 添加当事人记录
-	 * 
-	 * @author 尚
-	 * @param caseRegJObj
-	 * @return
-	 */
-	private int addConcerned(JSONObject caseRegJObj) {
-		int resultId = -1;
+        // 添加立案单
+        CaseRegistration caseRegistration =
+            JSON.parseObject(caseRegJObj.toJSONString(), CaseRegistration.class);
+        // 生成caseRegistration主键
+        String id = UUIDUtils.generateUuid();
+        caseRegistration.setId(id);
+        // 当事人主键
+        if (concernedId != -1) {
+            caseRegistration.setConcernedId(concernedId);
+        }
 
-		// 判断是否传入当事人信息
-		JSONObject concernedJObj = caseRegJObj.getJSONObject("concerned");
-		if (concernedJObj != null) {
-			// 有当事人信息
-			String concernedType = caseRegJObj.getString("concernedType");
+        // 添加文书
+        addWritsInstances(caseRegJObj, id);
 
-			switch (concernedType) {
-			case Constances.ConcernedStatus.ROOT_BIZ_CONCERNEDT_ORG:
-				// 当事人以单位形式存在
-				CLEConcernedCompany concernedCompany = JSON.parseObject(concernedJObj.toJSONString(),
-						CLEConcernedCompany.class);
-				cLEConcernedCompanyBiz.insertSelective(concernedCompany);
-				resultId = concernedCompany.getId();
-				break;
-			case Constances.ConcernedStatus.ROOT_BIZ_CONCERNEDT_PERSON:
-				// 当事人以人个形式存在
-				CLEConcernedPerson concernedPerson = JSON.parseObject(concernedJObj.toJSONString(),
-						CLEConcernedPerson.class);
-				cLEConcernedPersonBiz.insertSelective(concernedPerson);
-				resultId = concernedPerson.getId();
-				break;
-			}
-		}
+        this.insertSelective(caseRegistration);
+        // 将生成的立案ID装入procBizData带回工作流，在工作流中会对procBizId属性进行是否为“-1”的判断，如果是“-1”，将用该ID替换“-1”
+        caseRegJObj.put("procBizId", id);
 
-		return resultId;
-	}
+        result.setIsSuccess(true);
+        return result;
+    }
 
-	/**
-	 * 分页获取列表
-	 * 
-	 * @author 尚
-	 * @param ids
-	 * @param page
-	 * @param limit
-	 * @return
-	 */
-	public TableResultResponse<CaseRegistrationVo> getListByIds(String ids, int page, int limit) {
-		Set<String> idSet = new HashSet<>();
+    /**
+     * 将请求信息中文书信息进行保存
+     * 
+     * @param caseRegJObj
+     * @param id
+     */
+    private void addWritsInstances(JSONObject caseRegJObj, String id) {
+        WritsInstances writsInstances =
+            JSON.parseObject(caseRegJObj.getString("writsInstances"), WritsInstances.class);
+        if (writsInstances != null) {
+            /*
+             * 从请求参数中解析到的文已模板不为空，说明请求信息中有文书内容
+             * 在本处是对记录的初次添加，不需要对文书中fillContext内容进行处理，直接插入到数据库 中去
+             */
+            // 关联该文书相关的案件
+            writsInstances.setCaseId(id);
+            writsInstancesBiz.insertSelective(writsInstances);
+        }
+    }
 
-		String[] split = ids.split(",");
-		for (String string : split) {
-			idSet.add(string);
-		}
+    /**
+     * 添加当事人记录
+     * 
+     * @author 尚
+     * @param caseRegJObj
+     * @return 返回数据库自增长的ID，如果没有进行插入数据 操作则返回-1
+     */
+    private int addConcerned(JSONObject caseRegJObj) {
+        int resultId = -1;
 
-		Page<Object> pageInfo = PageHelper.startPage(page, limit);
-		List<CaseRegistrationVo> list = this.mapper.getListByIds(idSet, page, limit);
+        // 判断是否传入当事人信息
+        JSONObject concernedJObj = caseRegJObj.getJSONObject("concerned");
+        if (concernedJObj != null) {
+            // 有当事人信息
+            String concernedType = caseRegJObj.getString("concernedType");
 
-		return new TableResultResponse<CaseRegistrationVo>(pageInfo.getTotal(), list);
-	}
-	
-	/**
-	 * 按执法人分页查询对象
-	 * @param userId
-	 * @param page
-	 * @param limit
-	 * @return
-	 */
-	public TableResultResponse<CaseRegistration> getListByExecutePerson(String userId,int page,int limit){
-	    //enforcers
-	    TableResultResponse<CaseRegistration> restResult=new TableResultResponse<>();
-	    
-	    Example example=new Example(CaseRegistration.class);
-	    Criteria criteria = example.createCriteria();
-	    
-	    criteria.andEqualTo("isDeleted", "0");
-	    criteria.andEqualTo("enforcers", userId);
-	    
-	    Page<Object> pageInfo = PageHelper.startPage(page, limit);
-	    List<CaseRegistration> rows=this.selectByExample(example);
-	    
-	    restResult.getData().setTotal(pageInfo.getTotal());
-	    restResult.getData().setRows(rows);
-	    restResult.setStatus(200);
-	    return restResult;
-	}
+            switch (concernedType) {
+                case Constances.ConcernedStatus.ROOT_BIZ_CONCERNEDT_ORG:
+                    // 当事人以单位形式存在
+                    CLEConcernedCompany concernedCompany =
+                        JSON.parseObject(concernedJObj.toJSONString(), CLEConcernedCompany.class);
+                    cLEConcernedCompanyBiz.insertSelective(concernedCompany);
+                    resultId = concernedCompany.getId();
+                    break;
+                case Constances.ConcernedStatus.ROOT_BIZ_CONCERNEDT_PERSON:
+                    // 当事人以人个形式存在
+                    CLEConcernedPerson concernedPerson =
+                        JSON.parseObject(concernedJObj.toJSONString(), CLEConcernedPerson.class);
+                    cLEConcernedPersonBiz.insertSelective(concernedPerson);
+                    resultId = concernedPerson.getId();
+                    break;
+            }
+        }
+
+        return resultId;
+    }
+
+    /**
+     * 分页获取列表
+     * 
+     * @author 尚
+     * @param ids
+     * @param page
+     * @param limit
+     * @return
+     */
+    public TableResultResponse<CaseRegistrationVo> getListByIds(String ids, int page, int limit) {
+        Set<String> idSet = new HashSet<>();
+
+        String[] split = ids.split(",");
+        for (String string : split) {
+            idSet.add(string);
+        }
+
+        Page<Object> pageInfo = PageHelper.startPage(page, limit);
+        List<CaseRegistrationVo> list = this.mapper.getListByIds(idSet, page, limit);
+
+        return new TableResultResponse<CaseRegistrationVo>(pageInfo.getTotal(), list);
+    }
+
+    /**
+     * 按执法人分页查询对象
+     * 
+     * @param userId
+     * @param page
+     * @param limit
+     * @return
+     */
+    public TableResultResponse<CaseRegistration> getListByExecutePerson(String userId, int page,
+        int limit) {
+        // enforcers
+        TableResultResponse<CaseRegistration> restResult = new TableResultResponse<>();
+
+        Example example = new Example(CaseRegistration.class);
+        Criteria criteria = example.createCriteria();
+
+        criteria.andEqualTo("isDeleted", "0");
+        criteria.andEqualTo("enforcers", userId);
+
+        Page<Object> pageInfo = PageHelper.startPage(page, limit);
+        List<CaseRegistration> rows = this.selectByExample(example);
+
+        restResult.getData().setTotal(pageInfo.getTotal());
+        restResult.getData().setRows(rows);
+        restResult.setStatus(200);
+        return restResult;
+    }
+    
+    /**
+     * 按分页获取记录
+     * @param caseRegistration
+     * @param page
+     * @param limit
+     * @return
+     */
+    public TableResultResponse<CaseRegistration> getList(CaseRegistration caseRegistration,int page,int limit){
+        Example example =new Example(CaseRegistration.class);
+        Criteria criteria = example.createCriteria();
+        
+        criteria.andEqualTo("isDeleted", "0");
+        if(caseRegistration.getGirdId()!=null) {
+            criteria.andEqualTo("girdId", caseRegistration.getGirdId());
+        }
+        
+        example.setOrderByClause("id desc");
+        Page<Object> pageInfo = PageHelper.startPage(page, limit);
+        List<CaseRegistration> rows=this.selectByExample(example);
+        
+        return new TableResultResponse<>(pageInfo.getTotal(), rows);
+    }
 }
--- scp-modules/scp-cgp/src/main/java/com/bjzhianjia/scp/cgp/rest/CaseRegistrationController.java
+++ scp-modules/scp-cgp/src/main/java/com/bjzhianjia/scp/cgp/rest/CaseRegistrationController.java
@@ -1,5 +1,19 @@
 package com.bjzhianjia.scp.cgp.rest;
 
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alibaba.fastjson.JSONObject;
+import com.bjzhianjia.scp.cgp.biz.CaseRegistrationBiz;
+import com.bjzhianjia.scp.cgp.entity.CaseRegistration;
+import com.bjzhianjia.scp.cgp.entity.Result;
+import com.bjzhianjia.scp.security.auth.client.annotation.CheckClientToken;
+import com.bjzhianjia.scp.security.auth.client.annotation.CheckUserToken;
 import com.bjzhianjia.scp.security.common.msg.ObjectRestResponse;
 import com.bjzhianjia.scp.security.common.msg.TableResultResponse;
 import com.bjzhianjia.scp.security.common.rest.BaseController;
@@ -8,67 +22,66 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 
-import com.alibaba.fastjson.JSONObject;
-import com.bjzhianjia.scp.cgp.biz.CaseRegistrationBiz;
-import com.bjzhianjia.scp.cgp.entity.CaseRegistration;
-import com.bjzhianjia.scp.cgp.entity.Result;
-
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import com.bjzhianjia.scp.security.auth.client.annotation.CheckClientToken;
-import com.bjzhianjia.scp.security.auth.client.annotation.CheckUserToken;
-
 @RestController
 @RequestMapping("caseRegistration")
 @CheckClientToken
 @CheckUserToken
 @Api(tags = "综合执法 - 案件登记")
-public class CaseRegistrationController extends BaseController<CaseRegistrationBiz, CaseRegistration, String> {
+public class CaseRegistrationController
+    extends BaseController<CaseRegistrationBiz, CaseRegistration, String> {
 
-	@RequestMapping(value = "/add", method = RequestMethod.POST)
-	@ApiOperation("业务--添加单个对象")
-	public ObjectRestResponse<Void> addCase(
-			@RequestBody @ApiParam(name = "待添加对象实例") @Validated JSONObject caseRegJObj) {
-		ObjectRestResponse<Void> restResult = new ObjectRestResponse<>();
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    @ApiOperation("业务--添加单个对象")
+    public ObjectRestResponse<Void> addCase(
+        @RequestBody @ApiParam(name = "待添加对象实例") @Validated JSONObject caseRegJObj) {
+        ObjectRestResponse<Void> restResult = new ObjectRestResponse<>();
 
-		Result<Void> result = this.baseBiz.addCase(caseRegJObj);
-		if (!result.getIsSuccess()) {
-			restResult.setStatus(400);
-			restResult.setMessage(result.getMessage());
-			return restResult;
-		}
+        Result<Void> result = this.baseBiz.addCase(caseRegJObj);
+        if (!result.getIsSuccess()) {
+            restResult.setStatus(400);
+            restResult.setMessage(result.getMessage());
+            return restResult;
+        }
 
-		restResult.setMessage("成功");
-		return restResult;
-	}
+        restResult.setMessage("成功");
+        return restResult;
+    }
 
-	@RequestMapping(value = "/get")
-	@ApiOperation("查询 单条记录")
-	public ObjectRestResponse<JSONObject> getUserTaskDetail(
-			@RequestBody @ApiParam("封装查询条件的对象") @Validated JSONObject jobs, BindingResult bindingResult) {
-		ObjectRestResponse<JSONObject> restResult = new ObjectRestResponse<>();
+    @RequestMapping(value = "/get")
+    @ApiOperation("查询 单条记录")
+    public ObjectRestResponse<JSONObject> getUserTaskDetail(
+        @RequestBody @ApiParam("封装查询条件的对象") @Validated JSONObject jobs,
+        BindingResult bindingResult) {
+        ObjectRestResponse<JSONObject> restResult = new ObjectRestResponse<>();
 
-		if (bindingResult.hasErrors()) {
-			restResult.setMessage(bindingResult.getAllErrors().get(0).getDefaultMessage());
-			restResult.setStatus(400);
-			return restResult;
-		}
+        if (bindingResult.hasErrors()) {
+            restResult.setMessage(bindingResult.getAllErrors().get(0).getDefaultMessage());
+            restResult.setStatus(400);
+            return restResult;
+        }
 
-		return null;
-	}
-	
-	@RequestMapping(value="/list/enforcers",method=RequestMethod.GET)
-	@ApiOperation("按执法人分页查询对象")
-	public TableResultResponse<CaseRegistration> getListByExecutePerson(
-	    @RequestParam(value="userId",defaultValue="") @ApiParam("案件处理人ID")String userId,
-	    @RequestParam(value="page",defaultValue="1") @ApiParam("当前页")Integer page,
-	    @RequestParam(value="limit",defaultValue="10") @ApiParam("页容量")Integer limit){
-	    return this.baseBiz.getListByExecutePerson(userId, page, limit);
-	}
+        return null;
+    }
+
+    @RequestMapping(value = "/list/enforcers", method = RequestMethod.GET)
+    @ApiOperation("按执法人分页查询对象")
+    public TableResultResponse<CaseRegistration> getListByExecutePerson(
+        @RequestParam(value = "userId", defaultValue = "") @ApiParam("案件处理人ID") String userId,
+        @RequestParam(value = "page", defaultValue = "1") @ApiParam("当前页") Integer page,
+        @RequestParam(value = "limit", defaultValue = "10") @ApiParam("页容量") Integer limit) {
+        return this.baseBiz.getListByExecutePerson(userId, page, limit);
+    }
+    
+    @RequestMapping(value="/list",method=RequestMethod.GET)
+    @ApiOperation("分页获取对象列表")
+    public TableResultResponse<CaseRegistration> getList(
+        @RequestParam(value = "gridId", defaultValue = "") @ApiParam("网格ID") Integer gridId,
+        @RequestParam(value = "page", defaultValue = "1") @ApiParam("当前页") Integer page,
+        @RequestParam(value = "limit", defaultValue = "10") @ApiParam("页容量") Integer limit
+        ){
+        CaseRegistration caseRegistration=new CaseRegistration();
+        caseRegistration.setGirdId(gridId);
+        
+        return this.baseBiz.getList(caseRegistration, page, limit);
+    }
 }
