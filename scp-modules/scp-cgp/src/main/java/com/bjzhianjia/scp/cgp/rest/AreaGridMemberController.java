85d551edd9463d2ea9f4c553289f252cc9eef790
chenshuai <cs4380@163.com>
2018-09-20 18:38:27 +0800

    添加：通过用户id查询网信息接口

--- scp-modules/scp-cgp/src/main/java/com/bjzhianjia/scp/cgp/rest/AreaGridMemberController.java
+++ scp-modules/scp-cgp/src/main/java/com/bjzhianjia/scp/cgp/rest/AreaGridMemberController.java
@@ -1,5 +1,11 @@
 package com.bjzhianjia.scp.cgp.rest;
 
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,17 +23,31 @@ import com.bjzhianjia.scp.security.common.rest.BaseController;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 
+/**
+ * AreaGridMemberController 类描述.
+ *
+ *
+ * <pre>
+ * Modification History: 
+ * Date             Author      Version         Description 
+ * ------------------------------------------------------------------
+ * 2018年9月20日          chenshuai      1.0            ADD
+ * </pre>
+ * 
+ *
+ * @version 1.0
+ * @author chenshuai
+ *
+ */
 @RestController
 @RequestMapping("areaGridMember")
 @CheckClientToken
 @CheckUserToken
-public class AreaGridMemberController
-    extends BaseController<AreaGridMemberBiz, AreaGridMember, Integer> {
+public class AreaGridMemberController extends BaseController<AreaGridMemberBiz, AreaGridMember, Integer> {
 
     @RequestMapping(value = "/list", method = RequestMethod.GET)
     @ApiOperation("分页获取记录")
-    public TableResultResponse<JSONObject> getList(
-        @RequestParam(value = "memId", defaultValue = "") String memId,
+    public TableResultResponse<JSONObject> getList(@RequestParam(value = "memId", defaultValue = "") String memId,
         @RequestParam(value = "page", defaultValue = "1") Integer page,
         @RequestParam(value = "limit", defaultValue = "10") Integer limit) {
 
@@ -37,10 +57,34 @@ public class AreaGridMemberController
         return this.baseBiz.getList(areaGridMember, page, limit);
     }
 
-    @RequestMapping(value = "/get/member/detail",method=RequestMethod.GET)
+    @RequestMapping(value = "/get/member/detail", method = RequestMethod.GET)
     @ApiOperation("查询单个对象详情")
     public ObjectRestResponse<JSONObject> getDetailOfAeraMem(
         @RequestParam(value = "memID", defaultValue = "") @ApiParam("待查询网格员ID") String memId) {
         return this.baseBiz.getDetailOfAeraMem(memId);
     }
+
+    /**
+     * 通过用户id查询所属网格信息
+     * 
+     * @param userId
+     *            用户id
+     * @return
+     */
+    @GetMapping("/userId/{userId}")
+    @ApiOperation("通过用户id查询所属网格信息")
+    public ObjectRestResponse<List<Map<String, Object>>> getGridByUserId(
+        @PathVariable("userId") @ApiParam("待查询用户id") String userId) {
+
+        ObjectRestResponse<List<Map<String, Object>>> result = new ObjectRestResponse<>();
+        if (StringUtils.isBlank(userId)) {
+            result.setMessage("非法参数");
+            result.setStatus(400);
+            return result;
+        }
+        List<Map<String, Object>> data = this.baseBiz.getGridByUserId(userId);
+        result.setData(data);
+        return result;
+    }
+
 }
