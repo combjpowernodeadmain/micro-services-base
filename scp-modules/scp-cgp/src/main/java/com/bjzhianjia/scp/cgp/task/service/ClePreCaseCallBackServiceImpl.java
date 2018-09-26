a0d5d811c48b4e081920cf2a7b086e0225549253
尚村山夫 <13821211755@163.com>
2018-09-26 22:24:51 +0800

    案件登记，修改现场处罚登记逻辑

--- scp-modules/scp-cgp/src/main/java/com/bjzhianjia/scp/cgp/task/service/ClePreCaseCallBackServiceImpl.java
+++ scp-modules/scp-cgp/src/main/java/com/bjzhianjia/scp/cgp/task/service/ClePreCaseCallBackServiceImpl.java
@@ -8,8 +8,11 @@ import org.springframework.stereotype.Service;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.bjzhianjia.scp.cgp.biz.CaseRegistrationBiz;
+import com.bjzhianjia.scp.cgp.entity.CaseRegistration;
 import com.bjzhianjia.scp.security.wf.base.exception.BizException;
 import com.bjzhianjia.scp.security.wf.base.task.service.IWfProcTaskCallBackService;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * 处理执法立案逻辑的回调类
@@ -17,6 +20,7 @@ import com.bjzhianjia.scp.security.wf.base.task.service.IWfProcTaskCallBackServi
  * @author 尚
  */
 @Service
+@Slf4j
 public class ClePreCaseCallBackServiceImpl implements IWfProcTaskCallBackService {
 
     @Autowired
@@ -24,8 +28,24 @@ public class ClePreCaseCallBackServiceImpl implements IWfProcTaskCallBackService
 
     @Override
     public void before(String dealType, Map<String, Object> procBizData) throws BizException {
+        String bizType = String.valueOf(procBizData.get(PROC_BIZTYPE));
+        if (bizType == null) {
+            // 将bizTYpe置为空字符串，防止在switch时产生空指针，空字符串可进入default选项
+            bizType = "";
+        }
+        log.debug("*********************************案件办理，进入回调类程序*************************************");
+        log.debug("*********************************bizType:" + bizType + "*************************************");
+
         JSONObject bizData = JSONObject.parseObject(JSON.toJSONString(procBizData));
 
+        switch (bizType) {
+            case PROC_END:
+                bizData.put("exeStatus", CaseRegistration.EXESTATUS_STATE_FINISH);
+                break;
+            default:
+                bizData.put("exeStatus", CaseRegistration.EXESTATUS_STATE_TODO);
+                break;
+        }
         // 添加立案
         caseResistrationBiz.addCase(bizData);
         procBizData.put("procBizId", bizData.getString("procBizId"));
