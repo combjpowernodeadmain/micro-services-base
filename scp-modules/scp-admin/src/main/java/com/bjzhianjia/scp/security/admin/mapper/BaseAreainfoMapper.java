ee4d28baed240c825253b1146d09c35acc2ca8a0
chenshuai <cs4380@163.com>
2018-10-14 08:03:42 +0800

    添加：省级城市联动

---
+++ scp-modules/scp-admin/src/main/java/com/bjzhianjia/scp/security/admin/mapper/BaseAreainfoMapper.java
@@ -0,0 +1,23 @@
+package com.bjzhianjia.scp.security.admin.mapper;
+
+
+import com.bjzhianjia.scp.security.admin.entity.BaseAreainfo;
+import com.bjzhianjia.scp.security.common.mapper.CommonMapper;
+
+/**
+ * BaseAreainfoMapper 区县行政编码字典表.
+ *
+ *
+ * <pre>
+ * Modification History:
+ * Date             Author      Version         Description
+ * ------------------------------------------------------------------
+ * 2018年10月12日          bo      chenshuai            ADD
+ * </pre>
+ *
+ * @author chenshuai
+ * @version 1.0
+ */
+public interface BaseAreainfoMapper extends CommonMapper<BaseAreainfo> {
+
+}
