c2cb4e8e8130e9417072dcf873ce05bf68250ebc
chenshuai <cs4380@163.com>
2018-10-26 09:45:23 +0800

    添加：文件上传下载（LocalStorage）

--- scp-modules/scp-tool/src/main/java/com/bjzhianjia/scp/oss/controller/OssController.java
+++ scp-modules/scp-tool/src/main/java/com/bjzhianjia/scp/oss/controller/OssController.java
@@ -17,10 +17,19 @@
 
 package com.bjzhianjia.scp.oss.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
 
+import com.bjzhianjia.scp.config.CloudStorageConfig;
+import com.bjzhianjia.scp.oss.util.FileUtil;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,55 +44,84 @@ import com.bjzhianjia.scp.security.common.msg.ObjectRestResponse;
 
 /**
  * 文件上传
- * 
+ *
  * @author scp
  */
 @RestController
 @RequestMapping("/oss")
-public class OssController{
-	@Autowired
-	private OSSFactory ossFactory;
-	/**
-	 * 上传文件
-	 */
-	@RequestMapping("/upload")
-	public ObjectRestResponse<String> upload(@RequestParam("file") MultipartFile file) throws Exception {
-		if (file.isEmpty()) {
-			throw new BusinessException("上传文件不能为空");
-		}
-		//上传文件
-		String suffix = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf("."));
-		String url = "";
-		CloudStorageService storageService = ossFactory.build();
-		if(storageService instanceof LocalStorageService) {
-			url = ""; //TODO 添加本地文件存储
-		}else {
-			url = storageService.uploadSuffix(file.getBytes(), suffix);
-			
-		}
-		return new ObjectRestResponse<String>().data(url);
-	}
+public class OssController {
+    @Autowired
+    private OSSFactory ossFactory;
 
-	/**
-	 * 多文件上传
-	 */
-	@RequestMapping("/uploads")
-	public ObjectRestResponse<String> uploads(@RequestParam("files") MultipartFile[] files) throws Exception {
-		if (files == null || files.length < 0) {
-			throw new BusinessException("上传文件不能为空");
-		}
-		//上传文件
-		CloudStorageService storageService = ossFactory.build();
-		List<String>  urls = new ArrayList<>() ;
-		for (int i = 0; i < files.length; i++) {
-			MultipartFile file = files[i];
-			String suffix = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf("."));
-			String url = storageService.uploadSuffix(file.getBytes(), suffix);
-			urls.add(url);
-		}
-			
-		return new ObjectRestResponse<String>().data(urls.toString());
-	}
+    @Autowired
+    private CloudStorageConfig cloudStorageConfig;
 
+    /**
+     * 上传文件
+     */
+    @RequestMapping("/upload")
+    public ObjectRestResponse<String> upload(@RequestParam("file") MultipartFile file) throws Exception {
+        if (file.isEmpty()) {
+            throw new BusinessException("上传文件不能为空");
+        }
+        //上传文件
+        String suffix = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf("."));
+        String url;
+        CloudStorageService storageService = ossFactory.build();
+        if (storageService instanceof LocalStorageService) {
+            //添加本地文件存储
+            url = LocalStorageService.saveLocalStorage(file,cloudStorageConfig.getLocalStoragePathPrefix());
+        } else {
+            url = storageService.uploadSuffix(file.getBytes(), suffix);
 
+        }
+        return new ObjectRestResponse<String>().data(Base64.getUrlEncoder().encodeToString(url.getBytes()));
+    }
+
+    /**
+     * 多文件上传
+     */
+    @RequestMapping("/uploads")
+    public ObjectRestResponse<String> uploads(@RequestParam("files") MultipartFile[] files) throws Exception {
+        if (files == null || files.length < 0) {
+            throw new BusinessException("上传文件不能为空");
+        }
+        //上传文件
+        CloudStorageService storageService = ossFactory.build();
+        List<String> urls = new ArrayList<>();
+        String url = null;
+        for (int i = 0; i < files.length; i++) {
+            MultipartFile file = files[i];
+            String suffix = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf("."));
+            if (storageService instanceof LocalStorageService) {
+                //添加本地文件存储
+                url = LocalStorageService.saveLocalStorage(file,cloudStorageConfig.getLocalStoragePathPrefix());
+            } else {
+                url = storageService.uploadSuffix(file.getBytes(), suffix);
+            }
+            urls.add(url);
+        }
+        return new ObjectRestResponse<String>().data(Base64.getUrlEncoder().encodeToString(url.getBytes()));
+    }
+
+    /**
+     * 文件流下载
+     *
+     * @param path 文件路径（base64编码）
+     * @return
+     */
+    @RequestMapping(value = "/download/{path}")
+    public ResponseEntity<byte[]> download(@PathVariable("path") String path) throws IOException {
+        if (StringUtils.isBlank(path)) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        CloudStorageService storageService = ossFactory.build();
+        if (storageService instanceof LocalStorageService) {
+            //添加本地文件存储
+            File file = FileUtil.getLocalStorageFile(cloudStorageConfig.getLocalStoragePathPrefix(), path);
+            return FileUtil.download(file);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
