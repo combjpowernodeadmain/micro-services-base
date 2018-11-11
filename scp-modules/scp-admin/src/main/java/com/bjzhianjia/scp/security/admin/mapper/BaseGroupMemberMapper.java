7fc9be20ba2522fec0cdd05283ac13599040d68c
chenshuai <cs4380@163.com>
2018-11-11 12:52:29 +0800

    添加：用户列表中添加用户和角色关系维护

--- scp-modules/scp-admin/src/main/java/com/bjzhianjia/scp/security/admin/mapper/BaseGroupMemberMapper.java
+++ scp-modules/scp-admin/src/main/java/com/bjzhianjia/scp/security/admin/mapper/BaseGroupMemberMapper.java
@@ -2,6 +2,10 @@ package com.bjzhianjia.scp.security.admin.mapper;
 
 import com.bjzhianjia.scp.security.admin.entity.BaseGroupMember;
 import com.bjzhianjia.scp.security.common.mapper.CommonMapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * 
@@ -11,5 +15,18 @@ import com.bjzhianjia.scp.security.common.mapper.CommonMapper;
  * @version 2018-10-14 07:01:23
  */
 public interface BaseGroupMemberMapper extends CommonMapper<BaseGroupMember> {
-	
+
+    /**
+     * 用户角色列表
+     * @param userId 用户id
+     * @return
+     */
+    List<Map<String,Object>> selectNameByUserId(@Param("userId") String userId);
+
+    /**
+     *  删除用户角色关系
+     * @param userId 用户id
+     * @return
+     */
+    int deleteByUserId(@Param("userId") String userId);
 }
