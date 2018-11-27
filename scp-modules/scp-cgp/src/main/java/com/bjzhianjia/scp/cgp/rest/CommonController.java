c877acf0477b094ab07367c7e6a7143db4510fcb
尚村山夫 <13821211755@163.com>
2018-11-27 21:18:42 +0800

    双向推送

--- scp-modules/scp-cgp/src/main/java/com/bjzhianjia/scp/cgp/rest/CommonController.java
+++ scp-modules/scp-cgp/src/main/java/com/bjzhianjia/scp/cgp/rest/CommonController.java
@@ -1,21 +1,40 @@
 package com.bjzhianjia.scp.cgp.rest;
 
-import com.bjzhianjia.scp.security.auth.client.annotation.CheckClientToken;
-import com.bjzhianjia.scp.security.auth.client.annotation.CheckUserToken;
-import com.bjzhianjia.scp.security.common.msg.ObjectRestResponse;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import lombok.extern.slf4j.Slf4j;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import com.bjzhianjia.scp.cgp.biz.CaseRegistrationBiz;
+import com.bjzhianjia.scp.cgp.entity.Result;
+import com.bjzhianjia.scp.cgp.service.CaseInfoService;
+import com.bjzhianjia.scp.cgp.util.BeanUtil;
+import com.bjzhianjia.scp.security.auth.client.annotation.IgnoreClientToken;
+import com.bjzhianjia.scp.security.auth.client.annotation.IgnoreUserToken;
+
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.nio.charset.StandardCharsets;
+import com.alibaba.fastjson.JSONObject;
+import com.bjzhianjia.scp.security.auth.client.annotation.CheckClientToken;
+import com.bjzhianjia.scp.security.auth.client.annotation.CheckUserToken;
+import com.bjzhianjia.scp.security.common.msg.ObjectRestResponse;
+import com.bjzhianjia.scp.security.common.msg.TableResultResponse;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * CommonController 类描述. 用于处理通用的请求
@@ -43,6 +62,12 @@ public class CommonController {
 
     @Autowired
     private Environment environment;
+
+    @Autowired
+    private CaseInfoService caserInfoService;
+
+    @Autowired
+    private CaseRegistrationBiz caseRegistrationBiz;
 
     @GetMapping("/dictValue")
     @ApiOperation("用于请求dict库中code的值")
@@ -73,5 +98,145 @@ public class CommonController {
                 new String(property.getBytes(StandardCharsets.ISO_8859_1), StandardCharsets.UTF_8);
         }
         return new ObjectRestResponse<String>().data(property);
+    }
+
+    @PostMapping("/webDefault/caseInfo/allTasks")
+    @ApiOperation("事件综合查询")
+    @IgnoreClientToken
+    @IgnoreUserToken
+    public TableResultResponse<JSONObject> caseInfoAllTasks(@RequestBody JSONObject objs, HttpServletRequest request){
+        TableResultResponse<JSONObject> tableresult=new TableResultResponse<>();
+
+        Result<Void> voidResult = _checkAuth(objs);
+
+        if(!voidResult.getIsSuccess()){
+            tableresult.setMessage("无效认证");
+            tableresult.setStatus(401);
+            return tableresult;
+        }
+
+        TableResultResponse<JSONObject> tasks = caserInfoService.getAllTasks(objs);
+        return tasks;
+    }
+
+    @PostMapping("/webDefault/caseInfo/userTaskDetail")
+    @ApiOperation("事件综合查询")
+    @IgnoreClientToken
+    @IgnoreUserToken
+    public ObjectRestResponse<JSONObject> userCaseInfoTaskDetail(@RequestBody JSONObject objs, HttpServletRequest request){
+        ObjectRestResponse<JSONObject> objectResult=new ObjectRestResponse<>();
+
+        Result<Void> voidResult = _checkAuth(objs);
+        if(!voidResult.getIsSuccess()){
+            objectResult.setMessage("无效认证");
+            objectResult.setStatus(401);
+            return objectResult;
+        }
+
+        ObjectRestResponse<JSONObject> userToDoTask = caserInfoService.getUserToDoTask(objs);
+        return userToDoTask;
+    }
+
+    @PostMapping("/webDefault/caseRegistration/allTasks")
+    @ApiOperation("案件综合查询")
+    @IgnoreClientToken
+    @IgnoreUserToken
+    public TableResultResponse<JSONObject> caseRegistrationAllTasks(@RequestBody JSONObject objs, HttpServletRequest request){
+        TableResultResponse<JSONObject> tableresult=new TableResultResponse<>();
+
+        Result<Void> voidResult = _checkAuth(objs);
+
+        if(!voidResult.getIsSuccess()){
+            tableresult.setMessage("无效认证");
+            tableresult.setStatus(401);
+            return tableresult;
+        }
+
+        TableResultResponse<JSONObject> userToDoTasks = caseRegistrationBiz.getAllTasks(objs);
+        return userToDoTasks;
+    }
+
+    @PostMapping("/webDefault/caseRegistration/userTaskDetail")
+    @ApiOperation("事件综合查询")
+    @IgnoreClientToken
+    @IgnoreUserToken
+    public ObjectRestResponse<JSONObject> getUserCaseRegistrationToDoTask(@RequestBody JSONObject objs, HttpServletRequest request){
+        ObjectRestResponse<JSONObject> objectResult=new ObjectRestResponse<>();
+
+        Result<Void> voidResult = _checkAuth(objs);
+        if(!voidResult.getIsSuccess()){
+            objectResult.setMessage("无效认证");
+            objectResult.setStatus(401);
+            return objectResult;
+        }
+
+        if (objs != null) {
+            objectResult.setData(caseRegistrationBiz.getInfoById(objs));
+        } else {
+            objectResult.setStatus(400);
+            objectResult.setMessage("非法参数");
+        }
+
+        return objectResult;
+    }
+
+    private Result<Void> _checkAuth(JSONObject objs) {
+        Result<Void> result = new Result<>();
+        try {
+            String sign = objs.getString("sign");
+            objs.remove("sign");
+
+            List<String> authData2 = _getJObjSortStrings(objs.getJSONObject("authData"));
+            List<String> bizData2 = _getJObjSortStrings(objs.getJSONObject("bizData"));
+            List<String> procData2 = _getJObjSortStrings(objs.getJSONObject("procData"));
+            List<String> queryData2 = _getJObjSortStrings(objs.getJSONObject("queryData"));
+            List<String> variableData2 = _getJObjSortStrings(objs.getJSONObject("variableData"));
+
+            JSONObject objs02 = new JSONObject();
+            objs02.put("authData", String.join("", authData2));
+            objs02.put("bizData", String.join("", bizData2));
+            objs02.put("procData", String.join("", procData2));
+            objs02.put("queryData", String.join("", queryData2));
+            objs02.put("variableData", String.join("", variableData2));
+            objs02.put("secret", "it is a secret");
+
+            List<String> sortObjsList = _getJObjSortStrings(objs02);
+
+            String signOfServer =
+                DigestUtils.md5DigestAsHex(String.join("", sortObjsList).getBytes());
+            signOfServer = signOfServer.toUpperCase();
+
+            if (!sign.equals(signOfServer)) {
+                result.setMessage("无效认证");
+                result.setIsSuccess(false);
+                return result;
+            }
+
+            result.setIsSuccess(true);
+            return result;
+        } catch (Exception e) {
+            e.printStackTrace();
+            result.setMessage("无效认证");
+            result.setIsSuccess(false);
+            return result;
+        }
+    }
+
+    private List<String> _getJObjSortStrings(JSONObject jObj) {
+        List<String> jObjStrList = new ArrayList<>();
+        if (BeanUtil.isNotEmpty(jObj)) {
+            for (String queryKey : jObj.keySet()) {
+                jObjStrList.add(queryKey + "=" + jObj.get(queryKey));
+            }
+        }
+
+        jObjStrList.sort(new Comparator<String>() {
+
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.compareTo(o2);
+            }
+        });
+        return jObjStrList;
     }
 }
