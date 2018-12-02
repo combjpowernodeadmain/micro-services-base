2d74e2f3dbeea92a364224a8107b7ca7c8b10966
尚村山夫 <13821211755@163.com>
2018-12-02 23:29:14 +0800

    admin服务放行com.bjzhianjia.scp.security.admin.mapper.DepartMapper.selectByIds方法

--- scp-modules/scp-admin/src/main/java/com/bjzhianjia/scp/security/admin/config/MybatisDataConfig.java
+++ scp-modules/scp-admin/src/main/java/com/bjzhianjia/scp/security/admin/config/MybatisDataConfig.java
@@ -55,6 +55,7 @@ public class MybatisDataConfig {
             "com.bjzhianjia.scp.security.admin.mapper.UserMapper.selectLearderGroupCodesByUserId",
             "com.bjzhianjia.scp.security.admin.mapper.UserMapper.selectMemberGroupCodesByUserId",
                 "com.bjzhianjia.scp.security.admin.mapper.UserMapper.getUserDetail",
-                "com.bjzhianjia.scp.security.admin.mapper.UserMapper.selectByIds"));
+                "com.bjzhianjia.scp.security.admin.mapper.UserMapper.selectByIds"
+                ,"com.bjzhianjia.scp.security.admin.mapper.DepartMapper.selectByIds"));
     }
 }
