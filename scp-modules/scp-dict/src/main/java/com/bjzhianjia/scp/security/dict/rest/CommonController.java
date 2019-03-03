17be8d37abdc27367fe55c77d7d1aa1cbe362322
尚村山夫 <13821211755@163.com>
2019-03-03 15:29:16 +0800

    添加生成图片功能，用于指挥中心标题

--- scp-modules/scp-dict/src/main/java/com/bjzhianjia/scp/security/dict/rest/CommonController.java
+++ scp-modules/scp-dict/src/main/java/com/bjzhianjia/scp/security/dict/rest/CommonController.java
@@ -5,16 +5,21 @@ import com.bjzhianjia.scp.security.auth.client.annotation.CheckUserToken;
 import com.bjzhianjia.scp.security.auth.client.annotation.IgnoreClientToken;
 import com.bjzhianjia.scp.security.auth.client.annotation.IgnoreUserToken;
 import com.bjzhianjia.scp.security.common.msg.ObjectRestResponse;
+import com.bjzhianjia.scp.security.dict.utils.ImageUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 
 /**
@@ -66,5 +71,30 @@ public class CommonController {
         restResult.setStatus(200);
         restResult.setData(codeValue);
         return restResult;
+    }
+
+    @RequestMapping(value = "/commandCenterTitle", method = RequestMethod.GET)
+    @ApiOperation("生成指挥中心主页标题图片")
+    public void getCommandCenterTitle(
+        @RequestParam(value = "width",defaultValue = "578") @ApiParam(value = "图片宽度，默认578") Integer width,
+        @RequestParam(value = "heigth",defaultValue = "67") @ApiParam(value = "图片高度，默认67") Integer heigth,
+        @RequestParam(value = "frontType",defaultValue = "方正综艺简体") @ApiParam(value = "图片中字体，默认'方正综艺简体'") String frontType,
+        @RequestParam(value = "isBlod",defaultValue = "true") @ApiParam(value = "是否加粗，默认为'是'") boolean isBlod,
+        @RequestParam(value = "frontSize",defaultValue = "45") @ApiParam(value = "图片中字号，默认为45") Integer frontSize,
+        @RequestParam(value = "text") @ApiParam(value = "图片中内容，必传") String text,
+        @RequestParam(value = "r",defaultValue = "167") @ApiParam(value = "图片中文字颜色，默认167") Integer r,
+        @RequestParam(value = "b",defaultValue = "230") @ApiParam(value = "图片中文字颜色，默认230") Integer b,
+        @RequestParam(value = "g",defaultValue = "243") @ApiParam(value = "图片中文字颜色，默认243") Integer g,
+        @RequestParam(value = "imgFormatName",defaultValue = "png") @ApiParam(value = "图片格式，默认为'png'") String imgFormatName,
+        HttpServletResponse response) {
+        try {
+            Font font = new Font(frontType, isBlod?Font.BOLD:Font.PLAIN, frontSize);//字体
+
+            BufferedImage bi1 = ImageUtil
+                .generateImg(width, heigth, text, new Color(r, b, g), font, 0d, 1.0f);//给图片添加文字水印
+            ImageIO.write(bi1, imgFormatName, response.getOutputStream());//写入文件 )
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
