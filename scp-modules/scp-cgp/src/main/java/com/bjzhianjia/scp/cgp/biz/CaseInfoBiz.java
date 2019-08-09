c6c285375c4a8260fc02bea2b954cae42a2bc169
gongtan <13279429701@163.com>
2019-08-09 22:42:48 +0800

    1

--- scp-modules/scp-cgp/src/main/java/com/bjzhianjia/scp/cgp/biz/CaseInfoBiz.java
+++ scp-modules/scp-cgp/src/main/java/com/bjzhianjia/scp/cgp/biz/CaseInfoBiz.java
@@ -1357,6 +1357,9 @@ public class CaseInfoBiz extends BusinessBiz<CaseInfoMapper, CaseInfo> {
                     caseInfoIds.add(swpBizId.get(i).getString("bizId"));
                 }
             }
+            if(caseInfoIds.size() == 0){
+                caseInfoIds = null;
+            }
         }else{
             caseInfoIds = null;
         }
